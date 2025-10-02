45e58148b574bf6ad06f3e2f209a82a77c26d08e
Bart Massey <bart.massey@gmail.com>
2025-10-01 19:56:06 -0700

    added input parsing and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use fastrand as rand;
+use prompted::input;
 
 fn main() {
-    let n: u8 = rand::u8(1..=10);
-    println!("{}", n);
+    let target = rand::u8(1..=10);
+    let guess = input!("Guess the number between 1 and 10: ");
+    let guess: u8 = match guess.parse() {
+        Ok(number) => number,
+        Err(error) => {
+            println!("oops, not a number: {}", error);
+            return;
+        }
+    };
+    println!("{} {}", target, guess);
+    if guess == target {
+        println!("You win!");
+    } else {
+        println!("You lose!");
+    }
 }
