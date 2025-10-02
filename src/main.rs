0ba7c2417bcd99d036771ba4ae0dd25730dd696a
Bart Massey <bart.massey@gmail.com>
2025-10-01 20:00:19 -0700

    completed game

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,23 @@ use prompted::input;
 
 fn main() {
     let target = rand::u8(1..=10);
-    let guess = input!("Guess the number between 1 and 10: ");
-    let guess: u8 = match guess.parse() {
-        Ok(number) => number,
-        Err(error) => {
-            println!("oops, not a number: {}", error);
+    loop {
+        let guess = input!("Guess the number between 1 and 10: ");
+        let guess: u8 = match guess.parse() {
+            Ok(number) => number,
+            Err(error) => {
+                println!("oops, not a number: {}", error);
+                continue;
+            }
+        };
+        println!("{}", guess);
+        if guess == target {
+            println!("You win!");
             return;
+        } else if guess < target {
+            println!("Try higher.");
+        } else {
+            println!("Try lower.");
         }
-    };
-    println!("{} {}", target, guess);
-    if guess == target {
-        println!("You win!");
-    } else {
-        println!("You lose!");
     }
 }
